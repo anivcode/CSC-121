752de0cf55d2e36b8d38fd09973f2d491dd60169
anivcode <melanie.acue@gmail.com>
2023-09-26 16:18:00 -0500

    Update Shapes.cpp

--- Assignments/Shapes.cpp
+++ Assignments/Shapes.cpp
@@ -11,8 +11,8 @@ const short QUIT = 6;
 
 int main()
 {
-    short menu_choice;
-    double volume, side, radius, height, slant_height;
+    short menu_choice, second_choice;
+    double volume, surface_area, side, radius, height, slant_height;
 
     while (menu_choice != QUIT) { 
         cout << "1. Cube \n2. Sphere\n3. Prism\n4. Cylinder\n5. Cone\n6. Quit";
@@ -20,20 +20,34 @@ int main()
 
         switch (menu_choice) {
             case CUBE:
+                {
+                    cout << "A. Surface Area\nB.Volume";
+                    cin >> second_choice; 
+                    second_choice = static_cast<char>(toupper(second_choice));
+
+                    if (second_choice = 'A'){
+                        cout << "Please input one side. "; 
+                        cin >> side; 
+                        surface_area = ( 6*(side*side));
+                        cout << "The surface area is " << surface_area << " units squared.";
+                    }
+                    if (second_choice = 'B'){
+                        cout << "Please input one side. ";
+                        cin >> side;
+                        volume = (side * side * side); 
+                        cout << "The volume is" << volume << " units cubed."
+                   
+                    }
+                    }
 
 
 
-            case SPHERE: 
+
+                }
 
 
-            case PRISM:
 
 
-            case CYLINDER: 
-
-            case CONE: 
-
-            case QUIT:  
 
         }
 
